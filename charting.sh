36e8e838f917323a7fc6f4db3ed50bd807b440fd
Naman Nagar <naman.nagar@livspace.com>
2020-05-19 13:10:35 +0530

    added charting.sh

--- charting.sh
+++ charting.sh
@@ -1,4 +1,5 @@
 curl -o chart.zip -L https://github.com/livspaceeng/draft-charts/archive/master.zip
+ls -la
 mkdir charting
 unzip chart.zip -d charting
 rm -rf chart.zip
@@ -11,6 +12,8 @@ grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$app
 grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
 grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
 rm -rf charting
+ls -la
 cd charts/$appName
+ls -la
 make replace
 make release 

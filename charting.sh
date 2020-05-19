a135a91d99aa805701b0af76cf5ba4c3dffd0975
Naman Nagar <naman.nagar@livspace.com>
2020-05-19 14:44:49 +0530

    added charting.sh

--- charting.sh
+++ charting.sh
@@ -14,14 +14,15 @@ find . -depth -type d -name charts -execdir mv {} $appName \;
 mkdir charts
 mv $appName charts
 mv preview charts
-grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$appName/g" @
-grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
-grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
-rm -rf charting
-ls -la
 ls -la $pwd
-cd charts/$appName
-ls -la
-ls -la $pwd
-make replace
-make release 
+# grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$appName/g" @
+# grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
+# grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
+# rm -rf charting
+# ls -la
+# ls -la $pwd
+# cd charts/$appName
+# ls -la
+# ls -la $pwd
+# make replace
+# make release 

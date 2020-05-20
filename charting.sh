524c5235f259aea8052ed761aa9f6ab4d9857b8f
Naman Nagar <naman.nagar@livspace.com>
2020-05-20 16:16:02 +0530

    updated values.yaml

--- charting.sh
+++ charting.sh
@@ -3,7 +3,7 @@ pack=$1
 appName=$2
 org=$3
 tag=$4
-
+valuesFile=$5
 curl -o chart.zip -L https://github.com/livspaceeng/draft-charts/archive/master/${tag}.zip
 mkdir charting
 unzip chart.zip -d charting
@@ -13,16 +13,11 @@ find . -depth -type d -name charts -execdir mv {} $appName \;
 mkdir charts
 mv $appName charts
 mv preview charts
-cp ./values.yaml charts/$appName
-ls la 
-ls la $pwd
+cp ./$valuesFile charts/$appName
 grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$appName/g" @
 grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
 grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
 rm -rf charting
-ls -la $pwd
 cd charts/$appName
-ls -la
-ls -la $pwd
 make replace
 make release

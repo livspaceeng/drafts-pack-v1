0c7a8eee37b07043499d0a2b10d78a8113cdc55d
Naman Nagar <naman.nagar@livspace.com>
2020-05-20 15:30:19 +0530

    updated values.yaml

--- charting.sh
+++ charting.sh
@@ -14,6 +14,8 @@ mkdir charts
 mv $appName charts
 mv preview charts
 cp ./values.yaml charts/$appName
+ls la 
+ls la $pwd
 grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$appName/g" @
 grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
 grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
@@ -22,5 +24,5 @@ ls -la $pwd
 cd charts/$appName
 ls -la
 ls -la $pwd
-make replace
-make release                                                                                                                                                                                                                             
+# make replace
+# make release

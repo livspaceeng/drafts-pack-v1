bf464753da0a8c1720f4ee5070f6d73ac272f9f7
Naman Nagar <naman.nagar@livspace.com>
2020-05-20 14:07:42 +0530

    updated .gitignore file

--- charting.sh
+++ charting.sh
@@ -13,6 +13,7 @@ find . -depth -type d -name charts -execdir mv {} $appName \;
 mkdir charts
 mv $appName charts
 mv preview charts
+cp ./values.yaml charts/$appName
 grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$appName/g" @
 grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
 grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @

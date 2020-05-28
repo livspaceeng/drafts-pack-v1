a9a39a30fe4715cfd2451368a3e3ff63861c6814
Naman Nagar <naman.nagar@livspace.com>
2020-05-28 09:17:01 +0530

    Updated init.py

--- charting.sh
+++ charting.sh
@@ -24,6 +24,7 @@ else
 	grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
 	grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
 	rm -rf helmCharts
+	cd charts/$appName
 fi
 make replace
 make release

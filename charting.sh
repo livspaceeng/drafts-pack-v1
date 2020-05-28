0923c4c6f674e30d993099a32cd08025cf840620
Naman Nagar <naman.nagar@livspace.com>
2020-05-28 09:26:45 +0530

    Updated init.py

--- charting.sh
+++ charting.sh
@@ -6,25 +6,25 @@ org=$3
 tag=$4
 valuesFile=$5
 
-if [ -d charts];
-then 
-	cd charts/$appName
-else
-	curl -o chart.zip -L https://github.com/livspaceeng/draft-charts/archive/master/${tag}.zip
-	mkdir helmCharts
-	unzip chart.zip -d helmCharts
-	rm -rf chart.zip
-	mv helmCharts/draft-charts-master/packs/$pack/* ./
-	find . -depth -type d -name charts -execdir mv {} $appName \;
-	mkdir charts
-	mv $appName charts
-	mv preview charts
-	cp ./$valuesFile charts/$appName
-	grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$appName/g" @
-	grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
-	grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
-	rm -rf helmCharts
-	cd charts/$appName
-fi
-make replace
-make release
+# if [ -d charts];
+# then 
+# 	cd charts/$appName
+# else
+curl -o chart.zip -L https://github.com/livspaceeng/draft-charts/archive/master/${tag}.zip
+mkdir helmCharts
+unzip chart.zip -d helmCharts
+rm -rf chart.zip
+mv helmCharts/draft-charts-master/packs/$pack/* ./
+find . -depth -type d -name charts -execdir mv {} $appName \;
+mkdir charts
+mv $appName charts
+mv preview charts
+cp ./$valuesFile charts/$appName
+grep -rl 'REPLACE_ME_APP_NAME' ./ | xargs -I@ sed -i "s/REPLACE_ME_APP_NAME/$appName/g" @
+grep -rl 'REPLACE_ME_ORG' ./ | xargs -I@ sed -i "s/REPLACE_ME_ORG/$org/g" @
+grep -rl $pack charts/$appName/Chart.yaml | xargs -I@ sed -i "s/$pack/$appName/g" @
+rm -rf helmCharts
+cd charts/$appName
+# fi
+# make replace
+# make release
